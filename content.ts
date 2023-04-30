eb99b80559afcd8ff72090f8b5fc6ae52a3c5149
diffshare <diffshare@gmail.com>
2023-04-30 18:25:37 +0900

    add content_scripts config

--- content.ts
+++ content.ts
@@ -1,4 +1,10 @@
-export {}
+import type { PlasmoCSConfig } from "plasmo";
+
+export const config: PlasmoCSConfig = {
+  matches: ["*://meet.google.com/*"],
+  all_frames: true
+}
+
 console.log(
-    "You may find that having is not so pleasing a thing as wanting. This is not logical, but it is often true."
+  "You may find that having is not so pleasing a thing as wanting. This is not logical, but it is often true."
 )
