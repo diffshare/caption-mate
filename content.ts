2106181ffd79b494308953600584e76854168d10
diffshare <diffshare@gmail.com>
2023-05-02 22:48:14 +0900

    修正

--- content.ts
+++ content.ts
@@ -1,3 +1,4 @@
+import { sendToBackground } from "@plasmohq/messaging";
 import type { PlasmoCSConfig } from "plasmo";
 
 export const config: PlasmoCSConfig = {
@@ -5,70 +6,80 @@ export const config: PlasmoCSConfig = {
   all_frames: true
 }
 
-const divObserver = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    let text: string;
-    let user_name: string;
-    let image_src: string;
+const cache = {};
 
+const captionSpanObserver = new MutationObserver((mutations) => {
+  mutations.forEach(async (mutation) => {
     // 削除されたときに字幕の内容が確定されたものと判断する
-    if (mutation.removedNodes.length > 0) {
-      mutation.removedNodes.forEach((node) => {
-        if (node instanceof HTMLElement && node.tagName === "SPAN") {
-          const div = mutation.target.parentNode.parentNode;
-          if (div instanceof HTMLElement && div.tagName === "DIV" && div.children.length === 3) {
-            const img = div.children[0] as HTMLImageElement;
-            const user = div.children[1] as HTMLDivElement;
-            const text = div.children[2] as HTMLDivElement;
-            if (img.tagName === "IMG" && user.tagName === "DIV" && text.tagName === "DIV") {
-              user_name = user.innerText;
-              image_src = img.src;
-            }
+    mutation.removedNodes.forEach(async (node) => {
+      if (node instanceof HTMLElement && node.tagName === "SPAN") {
+        const div = mutation.target.parentNode.parentNode;
+        let speaker:{name: string, image: string};
+        if (div instanceof HTMLElement && div.tagName === "DIV" && div.children.length === 3) {
+          const img = div.children[0] as HTMLImageElement;
+          const user = div.children[1] as HTMLDivElement;
+          const text = div.children[2] as HTMLDivElement;
+          if (img.tagName === "IMG" && user.tagName === "DIV" && text.tagName === "DIV") {
+            speaker = {
+              name: user.innerText,
+              image: img.src
+            };
           }
-          text = node.innerText;
         }
-      });
-    }
-    if (!text || !user_name || !image_src) {
-      return;
-    }
-    const message = {
-      image_src,
-      user_name,
-      text: text
-    };
-    // この時点で字幕が確定したので、backgroundに送信
+        if (!speaker) return;
 
-    // TODO: ここでbackgroundに送信する
-    console.log({message})
+        const text = node.innerText;
+        if (mutation.previousSibling instanceof HTMLElement && mutation.previousSibling.tagName === "SPAN") {
+          const previousSiblingText = mutation.previousSibling.innerText;
+          cache[speaker.name + previousSiblingText] = text;
+          console.log({cache: text, previousSiblingText});
+        } else {
+          let completeText = text;
+          let query = speaker.name + text;
+          while (cache[query]) {
+            const nextText = cache[query];
+            delete cache[query];
+            completeText += nextText;
+            query = speaker.name + nextText;
+          }
+
+          console.log({completeText});
+          // この時点で字幕が確定したので、backgroundに送信
+          const resp = await sendToBackground({
+            name: "caption",
+            body: {
+              speaker,
+              text: completeText
+            }
+          })
+          console.log(resp);
+        }
+      }
+    });
   });
 });
-const bodyObserver = new MutationObserver((mutations) => {
+const captionDivObserver = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     if (mutation.addedNodes.length > 0) {
       mutation.addedNodes.forEach((node) => {
-        if (node instanceof HTMLElement && node.children.length === 3) {
-          if (node.tagName === "DIV") {
-            const img = node.children[0] as HTMLImageElement;
-            const user = node.children[1] as HTMLDivElement;
-            const text = node.children[2] as HTMLDivElement;
-            if (img.tagName === "IMG" && user.tagName === "DIV" && text.tagName === "DIV") {
-              // 字幕と思わしきタグの親要素の変更を監視
-              divObserver.observe(node.parentNode, {
-                attributes: true,
-                characterData: true,
-                childList: true,
-                subtree: true,
-              });
-            }
+        if (node instanceof HTMLElement && node.tagName === "DIV" && node.children.length === 3) {
+          const img = node.children[0] as HTMLElement;
+          const user = node.children[1] as HTMLElement;
+          const text = node.children[2] as HTMLElement;
+          if (img.tagName === "IMG" && user.tagName === "DIV" && text.tagName === "DIV") {
+            // 字幕と思わしきタグの祖父の要素に対してspanの削除を開始する
+            captionSpanObserver.observe(node.parentNode.parentNode, {
+              childList: true,
+              subtree: true,
+            });
+            captionDivObserver.disconnect();
           }
         }
       });
     }
   });
 });
-bodyObserver.observe(document.body, {
-  attributes: true,
+captionDivObserver.observe(document.body, {
   childList: true,
   subtree: true,
 });
